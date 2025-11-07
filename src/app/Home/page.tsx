b52e7efea980b777c201ea035eeb25969a33c516
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-07 15:22:23 +0530

    Added route

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Page from "../page";
+import UseMemoHook from "../useMemo";
+
+function page() {
+  return (
+    <div>
+      <Page />
+      <UseMemoHook />
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/useMemo.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+export default function UseMemoHook() {
+  const [num, setNum] = useState(0);
+  const [dark, setDark] = useState(false);
+
+  return <div>useMemo</div>;
+}
