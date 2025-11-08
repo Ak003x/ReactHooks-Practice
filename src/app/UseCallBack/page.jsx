69dc353e3600d78b9204a8b36bbbf5e724d7c489
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-08 20:50:47 +0530

    Added values

--- src/app/UseCallBack/page.jsx
+++ src/app/UseCallBack/page.jsx
@@ -6,16 +6,20 @@ export default function UseCallBackPage() {
   const [number, setNum] = useState(0);
   const [dark, setDark] = useState(false);
 
-  const getItems =useCallback( () => {
-    return [number + 1, number + 2, number + 3];
-  },[number]);
-
+  const getItems = useCallback(
+    (increment) => {
+      return [
+        number + increment + 1,
+        number + increment + 2,
+        number + increment + 3,
+      ];
+    },
+    [number]
+  );
 
   // const getItems =useMemo( () => {
   //   return [number + 1, number + 2, number + 3];
   // },[number]);
-
-
 
   const theme = {
     backgroundColor: dark ? "black" : "white",
